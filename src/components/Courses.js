ade97ab21d540004449911e27a44ee3c0a6561f0
James Crosslin <jamesmscrosslin@gmail.com>
2021-03-29 19:18:09 -0700

    set up routes and redirects

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
+import { NavLink, Route, Redirect } from "react-router-dom";
+import HTML from "./courses/HTML";
+import CSS from "./courses/CSS";
+import JavaScript from "./courses/JavaScript";
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li>
+          <NavLink to="/courses/html">HTML</NavLink>
+        </li>
+        <li>
+          <NavLink to="/courses/css">CSS</NavLink>
+        </li>
+        <li>
+          <NavLink to="/courses/javascript">JavaScript</NavLink>
+        </li>
       </ul>
     </div>
-    
+
     {/* Write routes here... */}
+
+    <Route
+      exact
+      path="/courses"
+      component={() => <Redirect to="/courses/html" />}
+    />
+    <Route path="/courses/html" component={() => <HTML />} />
+    <Route path="/courses/css" component={() => <CSS />} />
+    <Route path="/courses/javascript" component={() => <JavaScript />} />
   </div>
 );
 
